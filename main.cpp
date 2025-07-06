de43f2a77788457826318ded14924224152b67b9
VijaySP43 <vijaypattar7@gmail.com>
2025-07-06 23:11:38 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
+// main.cpp
 #include <iostream>
+
+void say_hello(); // Function declaration
+
 int main() {
     std::cout << "CI/CD Demo with GitHub Actions\n";
+    say_hello(); // Call function from hello.cpp
     return 0;
 }
